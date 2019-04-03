5c5934e4ea99913457d866f71987a0d0c94996d5
Nozeback <ajaltermatt@miuandes.cl>
2019-04-03 10:32:58 -0300

    falta agregar bien los numeros de telefono a User como Phone_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_length_of :password, minimum: 8 , maximum: 12
+##falta agregar los numeros
 end
+
