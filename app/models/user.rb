cdfc783454fb9729256f75fba06e510f2e505759
Nozeback <ajaltermatt@miuandes.cl>
2019-04-03 11:03:08 -0300

    algunos mas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :orders
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates_length_of :password, minimum: 8 , maximum: 12
+  validates :password, length:{in: 8..12}
+##validates :phone_number, length:{in: 8..12}
 ##falta agregar los numeros
 end
 
