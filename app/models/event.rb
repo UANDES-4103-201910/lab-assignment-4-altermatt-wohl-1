cf8b9a80a6267c131c7209bba61cae517dd90141
vawohlsillo <vawohl@miuandes.cl>
2019-04-04 01:29:09 -0300

    Revisar validacion y atributo de start_date en event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validate :start_date_cant_be_before_created_at
+
+  def start_date_cant_be_before_created_at
+    created_at = DateTime.now  
+    if start_date < created_at
+      errors.add(:start_date,"can't be before")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :orders
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :password, length:{in: 8..12}
-##validates :phone_number, length:{in: 8..12}
-##falta agregar los numeros
+  validates :number, length:{in: 8..12}
+
 end
 
