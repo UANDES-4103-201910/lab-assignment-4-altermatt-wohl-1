f66fce857dba2c7b26a89c1e78ceb54208f30002
vawohlsillo <vawohl@miuandes.cl>
2019-04-03 23:56:56 -0300

    correcciones

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,5 +1,5 @@
 class TicketType < ApplicationRecord
   belongs_to :event
   belongs_to :ticket_zone
-  validate :price, numerality: {greater_than_or_equal_to: 0}
+  validates :price, numerality: {greater_than_or_equal_to: 0}
 end
