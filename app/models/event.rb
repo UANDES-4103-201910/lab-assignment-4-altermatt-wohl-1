2163233887eb6b8de0d7da4071e158e672d37947
vawohlsillo <vawohl@miuandes.cl>
2019-04-05 00:03:15 -0300

    Final

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+  validates :start_date, uniqueness: { scope: :event_venue,
+    message: "The venue is occupied on that date" }
   validate :start_date_cant_be_before_created_at
 
   def start_date_cant_be_before_created_at
